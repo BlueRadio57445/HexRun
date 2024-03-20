1bfc94e90fb92e1c4499c14fba48801676af2d03
sky04281 <sky04282@gmail.com>
2024-03-20 20:55:33 +0800

    improve laser effect

--- Objects/laser.gd
+++ Objects/laser.gd
@@ -1,20 +1,32 @@
 extends Area2D
 
-var laser_width :float= 0
+var laser_width :float= 0 # 初始宽度
+var laser_length = 200 # 雷射长度
+var tween_duration = 1.0  # 动画持续时间（秒）
 @export var extend_speed :float= 20
 
 func _ready():
-	pass
+	var animation_player = $AnimationPlayer
+	animation_player.play("laser_animation")
+	
+	# 连接动画完成信号
+
+# 动画完成时的回调函数
+func _on_animation_finished(animation_name: String):
+	print("Animation finished:", animation_name)
+
 	
 func _init() -> void:
-	scale = Vector2(1,laser_width)
-	
+	pass
+#	scale = Vector2(1,laser_width)
+
 func _physics_process(delta):
-	#laser fire effect
-	if laser_width < 1:
-		laser_width += extend_speed * delta
-		scale = Vector2(1,laser_width)
+	pass
+#	#laser fire effect
+#	if laser_width < 1:
+#		laser_width += extend_speed * delta
+#		scale = Vector2(1,laser_width)
 	#firing for 0.5 seconds
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(0.7).timeout
 	queue_free()
 	
