36d2bf74137be5c75f5b3f31258d88dec5e0bb68
Blue_Radio <sky.magic5555@gmail.com>
2024-11-27 19:57:21 +0800

    選項不會重複了

--- Scenes/GameManager.gd
+++ Scenes/GameManager.gd
@@ -18,6 +18,84 @@ var option_icons = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	init_option_list()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func game_start():
+	score.show()
+	score.restart()
+	
+	$RoguelikeTimer.start()
+	
+func restart_game():
+	end_menu.hide()
+	GameManager.score.restart()
+	init_option_list()
+	lives.restart()
+	$RoguelikeTimer.start()
+	get_tree().reload_current_scene()
+	
+func end_game():
+	$RoguelikeTimer.stop()
+	end_menu.show()
+
+func resume_game():
+	roguelike_menu.hide()
+	$RoguelikeTimer.start()
+	get_tree().paused = false
+	pass
+
+func _on_roguelike_timer_timeout():
+	get_tree().paused = true
+	# 處理獎池的問題
+
+	# 生成三個不重複的隨機數，讓選項不要重複
+	var pool = generate_rand_list(3)
+
+	# 未來還要處理候選選項小於三個的狀況
+	
+	button_1_option = option_list[pool[0]]
+	button_2_option = option_list[pool[1]]
+	button_3_option = option_list[pool[2]]
+	roguelike_menu.change_icon(1, button_1_option["icon"])
+	roguelike_menu.change_icon(2, button_2_option["icon"])
+	roguelike_menu.change_icon(3, button_3_option["icon"])
+	roguelike_menu.show()
+	
+	print(len(option_list))
+	pass # Replace with function body.
+	
+	
+func on_roguelike_button_pressed(index:int):
+	match index:
+		1:
+			button_1_option["callable"].call()
+			option_list.erase(button_1_option)
+		2:
+			button_2_option["callable"].call()
+			option_list.erase(button_2_option)
+		3:
+			button_3_option["callable"].call()
+			option_list.erase(button_3_option)
+	pass
+
+func generate_rand_list(len=3) -> Array:
+	var pool:Array = []
+	for a in range(3):
+		var stop_randi:bool = false
+		while not stop_randi:
+			var _temp:int = randi_range(0,len(option_list)-1) ## 生成暫定數
+			if not _temp in pool: ## 找不到重複的數
+				pool.append(_temp)
+				stop_randi = true ## 中止無盡迴圈
+	return pool
+
+func init_option_list():
 	option_list.append(
 		{
 			"callable":function_list.blue_orange_hex,
@@ -38,72 +116,8 @@ func _ready():
 	)
 	option_list.append(
 		{
-			"callable":function_list.invulnerable_star,
-			"icon":load("res://Images/icon/invulnerable_star.png")
-		}
-	)
-	option_list.append(
-		{
 			"callable":function_list.three_lifes,
 			"icon":load("res://Images/icon/three_lifes.png")
 		}
 	)
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func game_start():
-	score.show()
-	score.restart()
-	
-	$RoguelikeTimer.start()
-	
-func restart_game():
-	end_menu.hide()
-	GameManager.score.restart()
-	lives.restart()
-	$RoguelikeTimer.start()
-	get_tree().reload_current_scene()
-	
-func end_game():
-	$RoguelikeTimer.stop()
-	end_menu.show()
-
-func resume_game():
-	roguelike_menu.hide()
-	$RoguelikeTimer.start()
-	get_tree().paused = false
-	pass
-
-func _on_roguelike_timer_timeout():
-	get_tree().paused = true
-	# 處理獎池的問題
-
-	# 未來再處理不要重複的問題，並且還要處理候選選項小於三個的狀況
-	
-	button_1_option = option_list.pick_random()
-	button_2_option = option_list.pick_random()
-	button_3_option = option_list.pick_random()
-	roguelike_menu.change_icon(1, button_1_option["icon"])
-	roguelike_menu.change_icon(2, button_2_option["icon"])
-	roguelike_menu.change_icon(3, button_3_option["icon"])
-	roguelike_menu.show()
-	pass # Replace with function body.
-	
-	
-func on_roguelike_button_pressed(index:int):
-	print(index)
-	match index:
-		1:
-			button_1_option["callable"].call()
-			option_list.erase(button_1_option)
-		2:
-			button_2_option["callable"].call()
-			option_list.erase(button_2_option)
-		3:
-			button_3_option["callable"].call()
-			option_list.erase(button_3_option)
 	pass
